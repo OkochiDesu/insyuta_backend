09e32004e36a3c1e9db98f9a66555455e5fc3800
masafumi <1994fumi1103@gmail.com>
2024-06-30 12:33:17 +0900

    first プロジェクト初回コミット

---
+++ src/main/kotlin/com/insyuta/backend/BackendProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.insyuta.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendProjectApplication>(*args)
+}
